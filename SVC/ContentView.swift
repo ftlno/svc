4e7d117fa702c9e0fce0f73efdbdfb51160d33ab
Fredrik Lillejordet <fretholil@gmail.com>
2021-10-19 22:05:09 +0200

    lagt til svc

--- SVC/ContentView.swift
+++ SVC/ContentView.swift
@@ -6,11 +6,23 @@
 //
 
 import SwiftUI
+import SafariServices
 
 struct ContentView: View {
+    @State var showSafari = false
+    @State var urlString = "https://www.bekk.no"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button(action: {
+            self.urlString = "https://www.bekk.no"
+            self.showSafari = true
+        }) {
+            Text("Bekk")
+        }
+        // summon the Safari sheet
+        .sheet(isPresented: $showSafari) {
+            SafariView(url:URL(string: self.urlString)!)
+        }
     }
 }
 
@@ -18,4 +30,18 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct SafariView: UIViewControllerRepresentable {
+    
+    let url: URL
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SFSafariViewController {
+        return SFSafariViewController(url: url)
+    }
+    
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariView>) {
+        
+    }
+    
 }
