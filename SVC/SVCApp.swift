5e49aad924ded1547cbfabccea2049a307f7aab9
Fredrik Lillejordet <fretholil@gmail.com>
2021-10-19 21:55:11 +0200

    Initial Commit

---
+++ SVC/SVCApp.swift
@@ -0,0 +1,17 @@
+//
+//  SVCApp.swift
+//  SVC
+//
+//  Created by Fredrik Lillejordet on 19/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct SVCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
